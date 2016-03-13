96387388f8ad51da15ee9877d11cb1e4b78df4d7
evan <evanjack@hotmail.com>
2016-03-13 17:04:32 +0800

    Add compiler flags
    Add TEST_U for loop unrolling
    Add TEST_M for MACRO
    Add TEST_S for SIMD (AVX)

--- Makefile
+++ Makefile
@@ -14,6 +14,19 @@ CFLAGS += $(PROF_FLAGS)
 LDFLAGS += $(PROF_FLAGS) 
 endif
 
+ifeq ($(strip $(TEST_U)),1)
+CFLAGS += -D_UNROLLING_
+endif
+
+ifeq ($(strip $(TEST_M)),1)
+CFLAGS += -D_MACRO_
+endif
+
+ifeq ($(strip $(TEST_S)),1)
+# -mavx -ftree-vectorize -ftree-vctorizer-verbose=1 -march=corei7-avx
+CFLAGS += -D_SIMD_ -mavx
+endif
+
 OBJS := \
 	objects.o \
 	raytracing.o \
@@ -38,6 +51,34 @@ use-models.h: models.inc Makefile
 	        -e 's/rectangular[0-9]/(\&&, \&rectangulars);/g' \
 	        -e 's/ = {//g' >> use-models.h
 
+gprof:
+	gprof ./raytracing | ../gprof2dot/gprof2dot.py -o gr.dot
+	dot -Tpng -o goutput.png gr.dot
+
+perf:
+	sudo sh -c " echo 0 > /proc/sys/kernel/kptr_restrict"
+	perf record -g -- ./raytracing
+	perf script | c++filt | ../gprof2dot/gprof2dot.py -f perf -o pr.dot
+	dot -Tpng -o poutput.png pr.dot
+
+rpt:
+	sudo sh -c " echo 0 > /proc/sys/kernel/kptr_restrict"
+	echo 1 | sudo tee /proc/sys/vm/drop_caches
+#	perf stat \
+#		-e cache-misses,cache-references,instructions,cycles,branches,branch-misses \
+#		./$(EXEC) 1>/dev/null
+	perf record \
+		-e cache-misses,cache-references,instructions,cycles,branches,branch-misses \
+		./$(EXEC)
+	perf report
+
+test: test.c
+	$(CC) $(CFLAGS) -o $@ $<
+
+run: $(EXEC)
+	./$(EXEC)
+	convert out.ppm out.png
+
 clean:
 	$(RM) $(EXEC) $(OBJS) use-models.h \
-		out.ppm gmon.out
+		out.ppm gmon.out *.png *.dot test
--- math-toolkit.h
+++ math-toolkit.h
@@ -5,6 +5,64 @@
 #include <stdio.h>
 #include <assert.h>
 
+#ifdef _SIMD_
+#include <immintrin.h>  // AVX
+#define ALIGN __attribute__ ((aligned (32)))
+#endif
+
+#ifdef _MACRO_
+static inline
+void normalize(double *v)
+{
+    double d = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+    assert(d != 0.0 && "Error calculating normal");
+
+    v[0] /= d;
+    v[1] /= d;
+    v[2] /= d;
+}
+
+#define length(v)   \
+    sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2])
+
+#define add_vector(a, b, out) \
+{   \
+    out[0] = a[0] + b[0];    \
+    out[1] = a[1] + b[1];    \
+    out[2] = a[2] + b[2];   \
+}
+
+#define subtract_vector(a, b, out) \
+{   \
+    out[0] = a[0] - b[0];    \
+    out[1] = a[1] - b[1];    \
+    out[2] = a[2] - b[2];   \
+}
+
+#define multiply_vectors(a, b, out) \
+{   \
+    out[0] = a[0] * b[0];    \
+    out[1] = a[1] * b[1];    \
+    out[2] = a[2] * b[2];   \
+}
+
+#define multiply_vector(a, b, out) \
+{   \
+    out[0] = a[0] * ((double)b);    \
+    out[1] = a[1] * ((double)b);    \
+    out[2] = a[2] * ((double)b);   \
+}
+
+#define cross_product(v1, v2, out) \
+{   \
+    out[0] = v1[1] * v2[2] - v1[2] * v2[1]; \
+    out[1] = v1[2] * v2[0] - v1[0] * v2[2]; \
+    out[2] = v1[0] * v2[1] - v1[1] * v2[0]; \
+}
+
+#define dot_product(v1, v2) \
+    ((v1[0] * v2[0]) + (v1[1] * v2[1]) + (v1[2] * v2[2]))
+#else
 static inline
 void normalize(double *v)
 {
@@ -25,29 +83,89 @@ double length(const double *v)
 static inline
 void add_vector(const double *a, const double *b, double *out)
 {
+#ifdef _UNROLLING_
+    out[0] = a[0] + b[0];
+    out[1] = a[1] + b[1];
+    out[2] = a[2] + b[2];
+#elif _SIMD_
+    double ALIGN c[4];
+    __m256d a1 = _mm256_set_pd(a[0], a[1], a[2], 0.0);
+    __m256d b1 = _mm256_set_pd(b[0], b[1], b[2], 0.0);
+    __m256d t1 = _mm256_add_pd(a1, b1);
+    _mm256_store_pd(c, t1);
+    out[0]=c[3];
+    out[1]=c[2];
+    out[2]=c[1];
+#else
     for (int i = 0; i < 3; i++)
         out[i] = a[i] + b[i];
+#endif
 }
 
 static inline
 void subtract_vector(const double *a, const double *b, double *out)
 {
+#ifdef _UNROLLING_
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2];
+#elif _SIMD_
+    double ALIGN c[4];
+    __m256d a1 = _mm256_set_pd(a[0], a[1], a[2], 0.0);
+    __m256d b1 = _mm256_set_pd(b[0], b[1], b[2], 0.0);
+    __m256d t1 = _mm256_sub_pd(a1, b1);
+    _mm256_store_pd(c, t1);
+    out[0]=c[3];
+    out[1]=c[2];
+    out[2]=c[1];
+#else
     for (int i = 0; i < 3; i++)
         out[i] = a[i] - b[i];
+#endif
 }
 
 static inline
 void multiply_vectors(const double *a, const double *b, double *out)
 {
+#ifdef _UNROLLING_
+    out[0] = a[0] * b[0];
+    out[1] = a[1] * b[1];
+    out[2] = a[2] * b[2];
+#elif _SIMD_
+    double ALIGN c[4];
+    __m256d a1 = _mm256_set_pd(a[0], a[1], a[2], 0.0);
+    __m256d b1 = _mm256_set_pd(b[0], b[1], b[2], 0.0);
+    __m256d t1 = _mm256_mul_pd(a1, b1);
+    _mm256_store_pd(c, t1);
+    out[0]=c[3];
+    out[1]=c[2];
+    out[2]=c[1];
+#else
     for (int i = 0; i < 3; i++)
         out[i] = a[i] * b[i];
+#endif
 }
 
 static inline
 void multiply_vector(const double *a, double b, double *out)
 {
+#ifdef _UNROLLING_
+    out[0] = a[0] * b;
+    out[1] = a[1] * b;
+    out[2] = a[2] * b;
+#elif _SIMD_
+    double ALIGN c[4];
+    __m256d a1 = _mm256_set_pd(a[0], a[1], a[2], 0.0);
+    __m256d b1 = _mm256_set1_pd(b);
+    __m256d t1 = _mm256_mul_pd(a1, b1);
+    _mm256_store_pd(c, t1);
+    out[0]=c[3];
+    out[1]=c[2];
+    out[2]=c[1];
+#else
     for (int i = 0; i < 3; i++)
         out[i] = a[i] * b;
+#endif
 }
 
 static inline
@@ -61,11 +179,25 @@ void cross_product(const double *v1, const double *v2, double *out)
 static inline
 double dot_product(const double *v1, const double *v2)
 {
+#if _UNROLLING_
+    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+#elif _SIMD_
+//    double ALIGN out[4];
+    __m256d a1 = _mm256_set_pd(v1[0], v1[1], v1[2], 0.0);
+    __m256d b1 = _mm256_set_pd(v2[0], v2[1], v2[2], 0.0);
+    __m256d t1 = _mm256_mul_pd(a1, b1);
+//    _mm256_store_pd(out, t1);
+//    return out[1]+out[2]+out[3];
+    return t1[1]+t1[2]+t1[3];
+#else
     double dp = 0.0;
+
     for (int i = 0; i < 3; i++)
         dp += v1[i] * v2[i];
     return dp;
+#endif
 }
+#endif
 
 static inline
 void scalar_triple_product(const double *u, const double *v, const double *w,
